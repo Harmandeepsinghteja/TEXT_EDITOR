536d31d1535357d2ec89792e7e6a78f55c023f12
Harmandeepsinghteja <harmandeepsinghteja@gmail.com>
2024-09-07 11:48:49 -0600

    Tunr off few more flags

--- kilo.c
+++ kilo.c
@@ -16,7 +16,10 @@ void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -26,9 +29,9 @@ int main() {
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
 	  if(iscntrl(c)){
-	    printf("%d\n", c);
+	    printf("%d\r\n", c);
 	  } else{
-	    printf("%d ('%c')\n",c ,c);
+	    printf("%d ('%c')\r\n",c ,c);
 	  }
 	  
 	}
